4038ae44dc974a718e3fb6669eef8b03a676df3a
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-18 18:09:38 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,80);
+$num = rand(1,21);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/dearxuan7392/picture/website/background/".$num.".jpg";
+$picpath = "https://gitlab.com/KINGWDY/papi/raw/main/".$num.".png";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
