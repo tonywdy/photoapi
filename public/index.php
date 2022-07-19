ad828afcb07764063d3e0850df8c97eaf9014313
wdy <106950730+tonywdy@users.noreply.github.com>
2022-07-19 21:31:37 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,45);
+$num = rand(1,21);
 //拼接图片地址
 $picpath = "https://gitlab.com/KINGWDY/photoapi/raw/main/".$num.".png";
 //重定位到图片
